b509734786bc9cd789c3cad3cb7288c497e24ffe
Chris DiFonzo <chris.difonzo@ghsiss.com>
2026-05-12 19:42:41 -0600

    Release v1.0.0

--- Package.swift
+++ Package.swift
@@ -15,13 +15,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "libEncoderLib",
-            url: "https://github.com/TrafficAuth/trafficauth-sdk-ios/releases/download/v1.0.1-rc1/libEncoderLib.xcframework.zip",
+            url: "https://github.com/TrafficAuth/trafficauth-sdk-ios/releases/download/v1.0.0/libEncoderLib.xcframework.zip",
             checksum: "6fdb04f5eb16e0c56cea0d116bbbb3e230a40f9129a1f1cb8c6655168c1abc29"
         ),
         .binaryTarget(
             name: "TrafficAuthSDK",
-            url: "https://github.com/TrafficAuth/trafficauth-sdk-ios/releases/download/v1.0.1-rc1/TrafficAuthSDK.xcframework.zip",
-            checksum: "bd22631d68361a0d0adf4dd390e60999a82402117b41de5eed7d6522ae619406"
+            url: "https://github.com/TrafficAuth/trafficauth-sdk-ios/releases/download/v1.0.0/TrafficAuthSDK.xcframework.zip",
+            checksum: "4f250a45fbc2eacd304a4096f2806d78ead881f6591574d1ba42a135aef041a9"
         )
     ]
 )
