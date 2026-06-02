fcbde89254dbfb4dda0d780ff4d38bae09971528
Andrew Chin <andrew.chin@ghsiss.com>
2026-06-02 11:59:37 -0400

    Release v1.0.4

--- Package.swift
+++ Package.swift
@@ -15,13 +15,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "libEncoderLib",
-            url: "https://github.com/TrafficAuth/trafficauth-sdk-ios/releases/download/v1.0.3/libEncoderLib.xcframework.zip",
-            checksum: "755d8e8eb3cedba2f259d545f98eb8483c47ab0ea696751e928ba163c97a2a00"
+            url: "https://github.com/TrafficAuth/trafficauth-sdk-ios/releases/download/v1.0.4/libEncoderLib.xcframework.zip",
+            checksum: "dd8d482ceb6141fa237350a5a8a228ed6c1650e24360caf37f4b0d4f8b0ef89f"
         ),
         .binaryTarget(
             name: "TrafficAuthSDK",
-            url: "https://github.com/TrafficAuth/trafficauth-sdk-ios/releases/download/v1.0.3/TrafficAuthSDK.xcframework.zip",
-            checksum: "4c04afd25fe0292e5db0a9be0176a79cde2decfc08128a70637c1d814aaf2524"
+            url: "https://github.com/TrafficAuth/trafficauth-sdk-ios/releases/download/v1.0.4/TrafficAuthSDK.xcframework.zip",
+            checksum: "6041b4dd275438495062959dabbb7f2fc76589a79bef902d44051bed7f6d6560"
         )
     ]
 )
