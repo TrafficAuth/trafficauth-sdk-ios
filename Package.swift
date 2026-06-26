3219be053ed3a0e7b69518d12cc0cc2c265148c4
Neil Hiddink <neil.hiddink@omnitrust.com>
2026-06-26 10:36:12 -0400

    Release v1.0.6

--- Package.swift
+++ Package.swift
@@ -15,13 +15,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "libEncoderLib",
-            url: "https://github.com/TrafficAuth/trafficauth-sdk-ios/releases/download/v1.0.5/libEncoderLib.xcframework.zip",
-            checksum: "090249508fdd841e9c4fb604d384de96d385fa937c3ee3650bfc491498b9e96a"
+            url: "https://github.com/TrafficAuth/trafficauth-sdk-ios/releases/download/v1.0.6/libEncoderLib.xcframework.zip",
+            checksum: "3fb0b84cbec52f80a8c4ef318ca2620aa64d7a4a9dd6d679976d614011b3ae80"
         ),
         .binaryTarget(
             name: "TrafficAuthSDK",
-            url: "https://github.com/TrafficAuth/trafficauth-sdk-ios/releases/download/v1.0.5/TrafficAuthSDK.xcframework.zip",
-            checksum: "b31f9dbbfff9833e3f5848ec653d3654d7375c3635f7abe547af92a458b009e4"
+            url: "https://github.com/TrafficAuth/trafficauth-sdk-ios/releases/download/v1.0.6/TrafficAuthSDK.xcframework.zip",
+            checksum: "437b58adf462e3fd0699ec1c9cd6b5d617d11709776fb96547f6b967cc592c35"
         )
     ]
 )
